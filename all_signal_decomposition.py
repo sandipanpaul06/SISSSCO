447b08588f616701696cf0b3ef0fb1888918e9ff
sandipanpaul06 <sarnab2020@fau.edu>
2022-07-22 22:20:51 -0400

    sumstat_ms bug fixed

--- all_signal_decomposition.py
+++ all_signal_decomposition.py
@@ -1,64 +1,20 @@
 import pandas as pd
 import numpy as np
 
-
-
-
-import argparse
-
-parser = argparse.ArgumentParser(description= 'Generate Time-frequency images for wavelet transform')
-parser.add_argument('sweep_filename', type=str, help= 'Sweep dataset filename')
-parser.add_argument('sweep_folder', type=str, help= 'Sweep dataset folder name')
-parser.add_argument('neutral_filename', type=str, help= 'Neutral dataset filename')
-parser.add_argument('neutral_folder', type=str, help= 'Neutral dataset folder name')
-parser.add_argument('train', type=int, help= 'Training samples')
-parser.add_argument('test', type=int, help= 'Test samples')
-parser.add_argument('val', type=int, help= 'validation samples')
-
-args = parser.parse_args()
-
-
-
-
-
-# In[40]:
-
-
-sw_file = args.sweep_filename
-sw_folder = args.sweep_foldername
-nt_file = args.neutral_filename
-nt_folder = args.neutral_foldername
-
-tr = args.train
-ts = args.test
-vl = args.val
-
-
-
-
-# In[33]:
-
-path1 = os.getcwd()
-
-
-
-
 """
 The three paths here needs to be changed
 """
-sweep = pd.read_csv(path1 + '/' + sw_folder + '/' + sw_file ,header= None)
-neutral = pd.read_csv(path1 + '/' + nt_folder + '/' + nt_file,header= None)
-#test = np.load("/mnt/beegfs/home/sarnab2020/Datasets/Empirical/signal_decomposition/22/chr22_1152_bed.npy")
+sweep = pd.read_csv("/mnt/beegfs/home/sarnab2020/Datasets/sweep_9_11000_ceu_hard.csv",header= None)
+neutral = pd.read_csv("/mnt/beegfs/home/sarnab2020/Datasets/neutral_9_11000_ceu_hard.csv",header= None)
+test = np.load("/mnt/beegfs/home/sarnab2020/Datasets/Empirical/signal_decomposition/22/chr22_1152_bed.npy")
 
-#TEST = pd.DataFrame(test)
+TEST = pd.DataFrame(test)
 
-All_classes_train_xy = pd.concat([sweep.iloc[:tr, :], neutral.iloc[:tr, :]])
-All_classes_val_xy = pd.concat([sweep.iloc[tr:tr+vl, :], neutral.iloc[tr:tr+vl, :]])
-All_classes_test_xy= pd.concat([sweep.iloc[tr+vl:tr+vl+ts, :], neutral.iloc[tr+vl:tr+vl+ts, :]])
+All_classes_train_xy = pd.concat([sweep.iloc[:9000, :], neutral.iloc[:9000, :]])
+All_classes_val_xy = pd.concat([sweep.iloc[9000:10000, :], neutral.iloc[9000:10000, :]])
+All_classes_test_xy= pd.concat([TEST.iloc[:1000, :], TEST.iloc[1000:10000, :]])
 
-#All_classes_test_xy= pd.concat([TEST.iloc[:1000, :], TEST.iloc[1000:10000, :]])
-
-label_col = ["Sweep"]*tr + ["Neutral"]*tr
+label_col = ["Sweep"]*9000 + ["Neutral"]*9000
 All_classes_train_xy["Label"] = label_col
 All_classes_train_xy= All_classes_train_xy.reset_index()
 del All_classes_train_xy['index']
@@ -66,7 +22,7 @@ All_classes_train_xy = pd.get_dummies(All_classes_train_xy)
 All_classes_train_xy = All_classes_train_xy.sample(frac=1, random_state=0)
 
 
-label_col = ["Sweep"]*vl + ["Neutral"]*vl
+label_col = ["Sweep"]*1000 + ["Neutral"]*1000
 All_classes_val_xy["Label"] = label_col
 All_classes_val_xy= All_classes_val_xy.reset_index()
 del All_classes_val_xy['index']
@@ -74,12 +30,12 @@ All_classes_val_xy = pd.get_dummies(All_classes_val_xy)
 All_classes_val_xy = All_classes_val_xy.sample(frac=1, random_state=2)
 
 
-label_col = ["Sweep"]*ts + ["Neutral"]*ts
+label_col = ["Sweep"]*1000 + ["Neutral"]*9000
 All_classes_test_xy["Label"] = label_col
 All_classes_test_xy= All_classes_test_xy.reset_index()
 del All_classes_test_xy['index']
 All_classes_test_xy = pd.get_dummies(All_classes_test_xy)
-#All_classes_test_xy = All_classes_test_xy.sample(frac=1, random_state=4)
+#All_classes_test_xy = All_classes_test_xy.sample(frac=1, random_state=0)
 
 summary_statistics = ["pi", "h1", "h12", "h2/h1", "f1", "f2", "f3", "f4", "f5"]
 
@@ -94,21 +50,25 @@ for x in range(9):
 
 import numpy as np
 
-spec_tensor_train = np.empty((2*tr, 65, 128, 9))
-spec_tensor_val = np.empty((2*vl, 65, 128, 9))
-spec_tensor_test = np.empty((2*ts, 65, 128, 9))
+spec_tensor_train = np.empty((18000, 65, 128, 9))
+spec_tensor_val = np.empty((2000, 65, 128, 9))
+spec_tensor_test = np.empty((10000, 65, 128, 9))
 
-import pywt
+from spectrum import *
+import numpy as np
+N=128
+NW=2.0
+k=65
+[tapers, eigen] = dpss(N, NW, k)
+
 '''
 for i in range(All_classes_stats_val["Stat_pi"].shape[0]):
   coefs_list = []
   for j in range(9):
-    signal = list(All_classes_stats_val["Stat_{0}".format(summary_statistics[j])].iloc[i,:])
-    scales= range(1, 66)
-    coefs, freqs = pywt.cwt(signal, scales, 'morl')
-    #squared = coefs ** 2
-    #sum_of_squared = np.sum(squared)
-    #coefs = coefs/sum_of_squared
+    signal = np.array(All_classes_stats_val["Stat_{0}".format(summary_statistics[j])].iloc[i,:])
+    Sk_complex, weights, eigenvalues=pmtm(signal, e=eigen, v=tapers, NFFT=128, show=False)
+
+    coefs = abs(Sk_complex)
     
     
 
@@ -129,11 +89,10 @@ for i in range(All_classes_stats_train["Stat_pi"].shape[0]):
   coefs_list = []
   for j in range(9):
     signal = list(All_classes_stats_train["Stat_{0}".format(summary_statistics[j])].iloc[i,:])
-    scales= range(1, 66)
-    coefs, freqs = pywt.cwt(signal, scales, 'morl')
-    #squared = coefs ** 2
-    #sum_of_squared = np.sum(squared)
-    #coefs = coefs/sum_of_squared
+    Sk_complex, weights, eigenvalues=pmtm(signal, e=eigen, v=tapers, NFFT=128, show=False)
+
+    coefs = abs(Sk_complex)
+    
     
 
     
@@ -147,18 +106,17 @@ for i in range(All_classes_stats_train["Stat_pi"].shape[0]):
       for coef in coefs_list:
         six_list.append(coef[k][l])
       spec_tensor_train[i][k][l] = np.array(six_list)
+
+
 '''
-
-
 for i in range(All_classes_stats_test["Stat_pi"].shape[0]):
   coefs_list = []
   for j in range(9):
     signal = list(All_classes_stats_test["Stat_{0}".format(summary_statistics[j])].iloc[i,:])
-    scales= range(1, 66)
-    coefs, freqs = pywt.cwt(signal, scales, 'morl')
-    #squared = coefs ** 2
-    #sum_of_squared = np.sum(squared)
-    #coefs = coefs/sum_of_squared
+    Sk_complex, weights, eigenvalues=pmtm(signal, e=eigen, v=tapers, NFFT=128, show=False)
+
+    coefs = abs(Sk_complex)
+    
     
 
     
@@ -240,35 +198,37 @@ for s in range(9):
             train_mean[0][row][col][s] = np.mean(pixel_array)
             train_SD[0][row][col][s] = np.std(pixel_array)
 '''
-train_mean = np.load("/mnt/beegfs/home/sarnab2020/Datasets/Empirical/train_mean_wavelet.npy")
-train_SD = np.load("/mnt/beegfs/home/sarnab2020/Datasets/Empirical/train_SD_wavelet.npy")
+train_mean = np.load("/mnt/beegfs/home/sarnab2020/Datasets/Empirical/train_mean_multitaper.npy")
+train_SD = np.load("/mnt/beegfs/home/sarnab2020/Datasets/Empirical/train_SD_multitaper.npy")
 '''
-scaled_spec_tensor_train = np.empty((2*tr, 65, 128, 9))
+scaled_spec_tensor_train = np.empty((18000, 65, 128, 9))
 
 for s in range(9):
     for row in range(65):
         for col in range(128):
             for sim in range(All_classes_stats_train["Stat_pi"].shape[0]):
                 scaled_spec_tensor_train[sim][row][col][s] = (spec_tensor_train[sim][row][col][s] - train_mean[0][row][col][s])/train_SD[0][row][col][s]
+
 '''
-scaled_spec_tensor_test = np.empty((2*ts, 65, 128, 9))
+scaled_spec_tensor_test = np.empty((10000, 65, 128, 9))
 
 for s in range(9):
     for row in range(65):
         for col in range(128):
             for sim in range(All_classes_stats_test["Stat_pi"].shape[0]):
                 scaled_spec_tensor_test[sim][row][col][s] = (spec_tensor_test[sim][row][col][s] - train_mean[0][row][col][s])/train_SD[0][row][col][s]
+
 '''
-scaled_spec_tensor_val = np.empty((2*vl, 65, 128, 9))
+scaled_spec_tensor_val = np.empty((2000, 65, 128, 9))
 
 for s in range(9):
     for row in range(65):
         for col in range(128):
             for sim in range(All_classes_stats_val["Stat_pi"].shape[0]):
                 scaled_spec_tensor_val[sim][row][col][s] = (spec_tensor_val[sim][row][col][s] - train_mean[0][row][col][s])/train_SD[0][row][col][s]
-
-np.save("train_SD_wavelet", train_SD)
-np.save("train_mean_wavelet", train_mean)
+                
+np.save("train_SD_multitaper", train_SD)
+np.save("train_mean_multitaper", train_mean)
 
 standardized_scalogram_sweep = np.empty((65, 128))
 standardizzed_scalogram_neutral = np.empty((65, 128))
@@ -313,18 +273,18 @@ fig.colorbar(im2, cax=cbar_ax)
 
 plt.savefig('Standardized.png')
 '''
-X_train_1 = scaled_spec_tensor_train.copy()
+#X_train_1 = scaled_spec_tensor_train.copy()
 X_test_1 = scaled_spec_tensor_test.copy()
-X_val_1 = scaled_spec_tensor_val.copy()
-Y_train_1 = np.array(y_train)
-Y_test_1 = np.array(y_test)
-Y_val_1 = np.array(y_val)
+#X_val_1 = scaled_spec_tensor_val.copy()
+#Y_train_1 = np.array(y_train)
+#Y_test_1 = np.array(y_test)
+#Y_val_1 = np.array(y_val)
 
-np.save("X_train_wavelet", X_train_1)
-np.save("X_test_wavelet", X_test_1)
-np.save("X_val_wavelet", X_val_1)
-np.save("Y_train", Y_train_1)
-np.save("Y_test", Y_test_0)
-np.save("Y_val", Y_val_1)
+#np.save("X_train_multitaper_hardceu", X_train_1)
+np.save("X_test_multitaper_emp_0", X_test_1)
+#np.save("X_val_multitaper_hardceu", X_val_1)
+#np.save("Y_train", Y_train_1)
+#np.save("Y_test", Y_test_0)
+#np.save("Y_val", Y_val_1)
             
 
